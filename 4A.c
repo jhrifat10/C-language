c230600907eb20ec29a0dd8df9201ff7a6b286f6
Jihadul Islam Rifat <jhrifat10@gmail.com>
2024-08-25 00:40:04 +0600

    4v.c

---
+++ 4A.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+int w;
+scanf("%d",&w);
+if  (1<=w<=100)
+{
+    if(w % 2==0)
+        printf("Yes");
+        else
+{
+       printf("No");
+
+}
+
+}
+
+    return 0;
+}
