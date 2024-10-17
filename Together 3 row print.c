aeecb3ab977c41aa96bdeb10cc7b5535b849b970
Jihadul Islam Rifat <jhrifat10@gmail.com>
2024-10-17 21:50:46 +0600

    Add files via upload

---
+++ Together 3 row print.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+int a,b,c,d;
+
+scanf("%d %d %d",&a,&b,&c);
+for(int i=a,j=b,k=c;i<=4;i++,j++,k++)
+{
+    printf("%d %d %d\n",i,j,k);
+}
+
+
+
+    return 0;
+}
